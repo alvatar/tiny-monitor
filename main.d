8231799962488a66bf8640b25d464e50adef3dd5
Álvaro Castro-Castilla <alvaro@castrocastilla.com>
2010-03-19 14:00:06 +0100

    Changed license to MIT

--- main.d
+++ main.d
@@ -1,5 +1,5 @@
 /+ Copyright (c) 2010 by Álvaro Castro-Castilla, All Rights Reserved.         +/
-/+ Licensed under the GPLv3 license, see LICENSE file for full description.   +/
+/+ Licensed under the MIT license, see LICENSE file for full description.     +/
 
 module monitor.main;
 
